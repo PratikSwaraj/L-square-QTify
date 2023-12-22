acdb942659087eccd8d10132e1943cff00be77ff
PratikSwaraj <pratikswaraj21@gmail.com>
2023-12-23 01:47:26 +0530

    Navbar

---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../../components/Button/Button";
+import Search from "../../components/Search/Search";
+import { ReactComponent as Logo } from "../../assets/qtifylogo.svg";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <Logo/>
+      <Search placeholder={"Search a album of your choice"}></Search>
+      <Button />
+    </div>
+  );
+}
+
+export default Navbar;
