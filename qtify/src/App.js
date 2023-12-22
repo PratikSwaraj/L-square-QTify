26468af5c2aba4245a2f550f37b7323dc3bb54d8
PratikSwaraj <pratikswaraj21@gmail.com>
2023-12-22 13:44:27 +0530

    Test commit

---
+++ qtify/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./components/Button/Button";
+import Search from "./components/Search/Search";
+
+
+function App() {
+  return (
+    <>
+     <Button>Give Feedback</Button>
+     <Search placeholder={"Search a album of your choice"}></Search>
+    </>
+  );
+}
+
+export default App;
---
+++ qtify/src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+function Button({children}) 
+{
+    return(
+        <>
+         <Button className={styles.button}>{children}</Button>
+        </>
+    )
+}
+
+export default Button;
